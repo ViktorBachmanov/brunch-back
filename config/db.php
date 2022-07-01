e01adfb4a80aa573e811196b9d7dd5e68254b0d5
Vikt0r <vbachmanov@mail.ru>
2022-07-01 12:28:27 +0300

    Replace MySql with PostgreSql

--- config/db.php
+++ config/db.php
@@ -2,9 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=brunch_test',
+    'username' => 'brunch_test',
+    'password' => 'brunch_test',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
