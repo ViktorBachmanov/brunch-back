87f0c24c123089706f25854efd34a1232b338eb9
Vikt0r <vbachmanov@mail.ru>
2022-07-03 18:18:48 +0300

    Replace users table with user one

---
+++ migrations/m220703_150621_create_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m220703_150621_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'email' => $this->string(64),
+            'name' => $this->string(64),
+            'tel' => $this->string(64),
+            'password' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
