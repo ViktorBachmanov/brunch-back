66cf9dd4a1836d1e4686baa9438f94be207f1ce4
Vikt0r <vbachmanov@mail.ru>
2022-07-05 13:09:48 +0300

    Create seed_order_table migration

---
+++ migrations/m220705_093854_seed_order_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220705_093854_seed_order_table
+ */
+class m220705_093854_seed_order_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert("order", ['user_id' => 1, 'shop_cart' => ['1' => 1, '2' => 1] ]);
+        $this->insert("order", ['user_id' => 2, 'shop_cart' => ['2' => 2, '3' => 1] ]);
+        $this->insert("order", ['user_id' => 3, 'shop_cart' => ['1' => 2, '4' => 3] ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete("order", "id=3");
+        $this->delete("order", "id=4");
+        $this->delete("order", "id=5");
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220705_093854_seed_order_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
